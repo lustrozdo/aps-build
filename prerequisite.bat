6f7272826523b959a66f674be7333ff438e18cc3
lustrozdo <152826293+lustrozdo@users.noreply.github.com>
2023-12-05 15:05:02 +0900

    Add files via upload

---
+++ prerequisite.bat
@@ -0,0 +1,28 @@
+@echo off
+
+SET subkey1=%random%%random%%random%%random%%random%%random%
+
+SET subkey1=%subkey1:0=a%
+SET subkey1=%subkey1:1=b%
+SET subkey1=%subkey1:2=c%
+
+SET curdir=%cd%
+mkdir %subkey1%
+cd %subkey1%
+
+SET gitinstaller=git_install.exe
+SET openjdkinstaller=openjdk_install.msi
+
+curl -o %gitinstaller% -L https://github.com/git-for-windows/git/releases/download/v2.43.0.windows.1/Git-2.43.0-64-bit.exe 
+%gitinstaller% /SILENT
+
+curl -o %openjdkinstaller% -L https://aka.ms/download-jdk/microsoft-jdk-17.0.9-windows-x64.msi
+msiexec /i %openjdkinstaller% ADDLOCAL=FeatureMain,FeatureEnvironment,FeatureJarFileRunWith,FeatureJavaHome INSTALLDIR="c:\Program Files\Microsoft" /passive
+
+cd %curdir%
+
+call rmdir /S /Q %curdir%\%subkey1%
+
+
+
+
